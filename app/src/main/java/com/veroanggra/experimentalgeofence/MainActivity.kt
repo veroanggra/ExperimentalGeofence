8937d3705114da3dcd3e5bbad8ae830433345889
Veronica Putri Anggraini <veronicaputri497@gmail.com>
2022-10-10 11:53:16 +0700

    create trigger when permssion not granted

--- app/src/main/java/com/veroanggra/experimentalgeofence/MainActivity.kt
+++ app/src/main/java/com/veroanggra/experimentalgeofence/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.veroanggra.experimentalgeofence
 
+import android.Manifest
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
+import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.GeofencingClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.veroanggra.experimentalgeofence.databinding.ActivityMainBinding
+import com.veroanggra.experimentalgeofence.util.PermissionHelper
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -36,6 +40,26 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
     override fun onMapReady(p0: GoogleMap) {
-        TODO("Not yet implemented")
+        map = p0
+        map.uiSettings.isZoomControlsEnabled = true
+
+        if (!PermissionHelper.hasPermission(this, locationPermissions)) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                locationPermissions.add(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+            }
+            ActivityCompat.requestPermissions(this, locationPermissions.toTypedArray(), LOCATION_REQUEST_CODE)
+        } else {
+            if (PermissionHelper.getDeniedPermission(this, locationPermissions)) {
+                return
+            }
+        }
+    }
+
+    companion object {
+        val locationPermissions = mutableListOf<String>(
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
+        const val LOCATION_REQUEST_CODE = 123
     }
 }
