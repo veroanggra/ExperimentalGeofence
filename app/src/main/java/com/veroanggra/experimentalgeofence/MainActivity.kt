028d4da307bf7f8a0d7e6cdce7ca89d6a70d0aff
Veronica Putri Anggraini <veronicaputri497@gmail.com>
2022-10-10 07:20:38 +0700

    initialize maps activity

--- app/src/main/java/com/veroanggra/experimentalgeofence/MainActivity.kt
+++ app/src/main/java/com/veroanggra/experimentalgeofence/MainActivity.kt
@@ -3,13 +3,27 @@ package com.veroanggra.experimentalgeofence
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.GeofencingClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
 import com.veroanggra.experimentalgeofence.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
     private lateinit var binding: ActivityMainBinding
+    private lateinit var map: GoogleMap
+    private lateinit var geoFencingClient: GeofencingClient
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        geoFencingClient = LocationServices.getGeofencingClient(this)
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map_view)
+        setContentView(binding.root)
     }
 
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
@@ -18,6 +32,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun checkPermissionsStartgeofencing() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onMapReady(p0: GoogleMap) {
         TODO("Not yet implemented")
     }
 }
